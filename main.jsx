bdef73e799bbbf7d4bb59ef3701404c8e772efd5
SHAILENDRA <100473483+shailendra043@users.noreply.github.com>
2025-11-27 18:23:42 +0530

    hi

--- main.jsx
+++ main.jsx
@@ -2,9 +2,9 @@ import React from 'react'
 import { createRoot } from 'react-dom/client'
 import { BrowserRouter, Routes, Route, Link } from 'react-router-dom'
 import App from './App'
-import FormBuilder from './pages/FormBuilder'
-import FormViewer from './pages/FormViewer'
-import ResponsesList from './pages/ResponsesList'
+import FormBuilder from './FormBuilder'
+import FormViewer from './FormViewer'
+import ResponsesList from './ResponsesList'
 
 createRoot(document.getElementById('root')).render(
   <BrowserRouter>
